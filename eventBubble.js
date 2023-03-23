d1c97f13f07a0fd7b1d656c9f98585a8e0f1578b
Irfan Ahmed <121118460+irfanahmed812@users.noreply.github.com>
2023-03-23 16:39:49 +0600

    Add files via upload

---
+++ eventBubble.js
@@ -0,0 +1,29 @@
+document.getElementById('second-item').addEventListener('click', function (event) {
+    console.log('second items clicked');
+    event.stopImmediatePropagation();
+    // event.stopPropagation();
+})
+document.getElementById('second-item').addEventListener('click', function (event) {
+    console.log('second items clicked');
+    // event.stopPropagation();
+})
+document.getElementById('second-item').addEventListener('click', function (event) {
+    console.log('second items clicked');
+    // event.stopPropagation();
+})
+document.getElementById('second-item').addEventListener('click', function (event) {
+    console.log('second items clicked');
+    // event.stopPropagation();
+})
+document.getElementById('second-item').addEventListener('click', function (event) {
+    console.log('second items clicked');
+    // event.stopPropagation();
+})
+
+document.getElementById('list-container').addEventListener('click', function (event) {
+    console.log('list-contianer clicked');
+    event.stopPropagation
+})
+document.getElementById('new').addEventListener('click', function () {
+    console.log('new clicked');
+})
